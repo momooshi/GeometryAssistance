a048f3db239694bec74f78bad3daa1903f11a3b9
oshi <aaaaa>
2020-07-02 21:30:33 +0300

    First commit

---
+++ Round/round.cpp
@@ -0,0 +1,5 @@
+#include "round.h"
+
+Round::Round()
+{
+}
---
+++ Round/round.h
@@ -0,0 +1,10 @@
+#ifndef ROUND_H
+#define ROUND_H
+#include "../Figure/figure.h"
+class Round: protected Figure
+{//массив sides имеет один элемент - длину окружности, добавляется переменная радиуса
+public:
+    Round();
+};
+
+#endif // ROUND_H
