04e055392ea8c68917c06d1d2a67d01aed3642b6
Ekaterina Evdokimova <67654181+momooshi@users.noreply.github.com>
2020-07-08 16:46:20 +0300

    Update Rrectangle.h

--- Quadra/Rrectangle.h
+++ Quadra/Rrectangle.h
@@ -9,62 +9,29 @@ class Rrectangle:public Figure
 {
   private:
   double width, length; //длина и ширина прямоугольника
-  double a=90;          //углы прямоугольника
-  double b=90;
-  double c=90;
-  double d=90;
+  const int angle = 90;
 
   public:
-    void set_width(double a)
-    {
-        width=a;
-    }
-    void set_length(double a)
-    {
-        length=a;
-    }
-    double get_width()
-    {
-        return width;
-    }
-    double get_length()
-    {
-        return length;
-    }
-    double get_square()
-    {
-        return square;
-    }
-    double count_perimeter()
-    {
-       return width*2+length*2;
-    }
-    double get_perimeter()
-    {
-        return perimeter;
-    }
-    double count_square()
-    {
-        return width*length;
-    }
-    double get_angle_a()
-    {
-        return a;
-    }
-    double get_angle_b()
-    {
-        return b;
-    }
-    double get_angle_c()
-    {
-        return c;
-    }
-    double get_angle_d()
-    {
-        return d;
-    }
+    void count_perimeter() { perimeter = width*2+length*2; }
+    void count_square() { square = width*length; }
+    
+    void set_width(double t_width) { width = t_width; }
+    void set_length(double t_length) { length = t_length; }
+    
+    double get_width() { return width; }
+    double get_length() { return length; }
+    double get_square() { return square; }  
+    double get_perimeter() { return perimeter; }  
+    int get_angle() { return angle; }
+
     QString get_name() { return name; }
-    Rrectangle();
+    Rrectangle(): width (0), length (0)
+    {}
+    Rrectangle (double t_width, double t_length): width (t_width), length (t_length)
+    {
+     count_perimeter();
+     count_square ();
+    }    
     ~Rrectangle();
     };
 
