fbc4b4cb0144936e2016bc9c5e55fa1f94994d7b
Podoprikhina <t_podoprikhina@mail.ru>
2020-07-13 11:54:10 +0300

    Edited Triangle library

--- LibraryTriangle/librarytriangle.cpp
+++ LibraryTriangle/librarytriangle.cpp
@@ -1,107 +1,100 @@
 #include "librarytriangle.h"
 
-
-LibraryTriangle::LibraryTriangle()
+Triangle::Triangle()
 {
-    side1=0;
-    side2=0;
-    side3=0;
-    angle1=0;
-    angle2=0;
-    angle3=0;
+name = "Треугольник";
+side1=0;
+side2=0;
+side3=0;
+angle1=0;
+angle2=0;
+angle3=0;
+}
+Triangle::Triangle (double t_side1, double t_side2, double t_side3): side1(t_side1), side2(t_side2), side3(t_side3)
+{
+name="Треугольник";
+count_3angle_on_3side();
+count_square();
+count_perimeter();
+
+}
+Triangle::Triangle(double t_side1, double t_side2, int t_angle1) : side1(t_side1), side2(t_side2), angle1(t_angle1)
+{
+count_on_1angle_2side();
+count_perimeter();
+count_square();
+}
+void Triangle::count_side1_sidesangle()
+{
+side1=sqrt(side2*side2+side3*side3-2*side2*side3*cos(angle2));
+
 }
 
-void LibraryTriangle::set_side1(double a)
+void Triangle::count_angles()
+{ angle1 = acos((side1*side1+side2*side2-side3*side3)/(2*side1*side2))*180.0/M_PI;
+angle2 = acos((side2*side2+side3*side3-side1*side1)/(2*side2*side3))*180.0/M_PI;
+angle3 = acos((side1*side1+side3*side3-side2*side2)/(2*side1*side3))*180.0/M_PI;
+}
+void Triangle::count_on_2angle_1side()
 {
-   side1=a;
-   perimeter=count_perimeter();
-   square=count_square();
-   count_angles_cos();
+angle3=180-(angle1+angle2);
+side2=side1*(sin(angle2*M_PI/180.0)/sin(angle1*M_PI/180.0));
+side3=side1*(sin(angle3*M_PI/180.0)/sin(angle1*M_PI/180.0));
 }
 
-void LibraryTriangle::set_side2(double a)
+void Triangle::count_on_1angle_2side()
 {
-   side2=a;
-   perimeter=count_perimeter();
-   square=count_square();
-   count_angles_cos();
+side3=sqrt(side1*side1+side2*side2-2*side1*side2*cos(angle1*M_PI/180));
+angle2 = acos((side2*side2+side3*side3-side1*side1)/(2*side2*side3))*180.0/M_PI;
+angle3 = acos((side1*side1+side3*side3-side2*side2)/(2*side1*side3))*180.0/M_PI;
+
 }
 
-void LibraryTriangle::set_side3(double a)
+int Triangle::count_3angle_on_3side()
 {
-   side3=a;
-   perimeter=count_perimeter();
-   square=count_square();
-   count_angles_cos();
+if (square)
+{
+angle1 = asin((2*square/(side1*side2)))*180.0/M_PI;
+angle2 = asin((2*square/(side2*side3)))*180.0/M_PI;
+angle3 = asin((2*square/(side3*side1)))*180.0/M_PI;
+return 0;
+}
+else if ((side1<(side2+side3))||(side2<(side1+side3))||(side3<(side1+side2)))
+{
+angle1=acos((side2*side2+side3*side3-side1*side1)/2*side2*side3)*180/M_PI;
+angle2=acos((side1*side1+side3*side3-side2*side2)/2*side1*side3)*180/M_PI;
+angle3=180-(angle1+angle2);
+return 0;
+}
+else {return -1;}
+
+}
+void Triangle::set_side1(double a)
+{
+side1=a;
 }
 
-void LibraryTriangle::set_angle1(double b)
+void Triangle::set_side2(double a)
 {
-   angle1=b;
-   perimeter=count_perimeter();
-   square=count_square();
-   count_on_1angle_2side();
+side2=a;
 }
 
-void LibraryTriangle::set_angle2(double b)
+void Triangle::set_side3(double a)
 {
-   angle2=b;
-   perimeter=count_perimeter();
-   square=count_square();
-   count_on_1angle_2side();
+side3=a;
 }
 
-void LibraryTriangle::set_angle3(double b)
+void Triangle::set_angle1(double b)
 {
-   angle3=b;
-   perimeter=count_perimeter();
-   square=count_square();
-   count_on_1angle_2side();
+angle1=b;
 }
 
-double LibraryTriangle::count_side1_sidesangle()
+void Triangle::set_angle2(double b)
 {
-    side1=sqrt(side2*side2+side3*side3-2*side2*side3*cos(angle2));
-    return side1;
+angle2=b;
 }
 
-double LibraryTriangle::count_third_angle()
+void Triangle::set_angle3(double b)
 {
-    angle3=180-angle1-angle2;
-    return angle3;
-}
-
-void LibraryTriangle::count_angles_cos()
-{
-    angle1=acos((side2*side2+side3*side3-side1*side1)/2*side2*side3);
-    angle2=acos((side1*side1+side3*side3-side2*side2)/2*side1*side2);
-    angle3=acos((side1*side1+side2*side2-side3*side3)/2*side1*side3);
-}
-
-void LibraryTriangle::count_on_2angle_1side()
-{
-    angle1=180-(angle2+angle3);
-    side2=side1*(sin(angle2)/sin(angle1));
-    side3=side1*(sin(angle3)/sin(angle1));
-}
-
-double LibraryTriangle::count_on_1angle_2side()
-{
-    side3=sqrt(side1*side1+side2*side2-2*side1*side2*cos(angle3));
-    angle1=acos((side2*side2+side3*side3-side1*side1)/2*side2*side3);
-    angle2=acos((side1*side1+side3*side3-side2*side2)/2*side1*side3);
-    return side3;
-}
-
-double LibraryTriangle::count_3angle_on_3side()
-{
-    if ((side1<(side2+side3))||(side2<(side1+side3))||(side3<(side1+side2)))
-    {
-        angle1=acos((side2*side2+side3*side3-side1*side1)/2*side2*side3);
-        angle2=acos((side1*side1+side3*side3-side2*side2)/2*side1*side3);
-        angle3=180-(angle1+angle2);
-    }
-    else {return 666;}
-
-    return 0;
+angle3=b;
 }
--- LibraryTriangle/librarytriangle.h
+++ LibraryTriangle/librarytriangle.h
@@ -1,63 +1,46 @@
 #ifndef LIBRARYTRIANGLE_H
 #define LIBRARYTRIANGLE_H
-#include  "../Figure/figure.h "
+#include "../Figure/figure.h "
 
-#include  <cmath>
-#include <iostream>
-
-
+#include <cmath>
 class Triangle : public Figure
 {
-  private:
-    double side1, side2, side3;
-    double angle1, angle2, angle3;
+private:
+double side1, side2, side3;
+double angle1, angle2, angle3;
 
-  public:
-    double count_perimeter();
-    double count_square();
+public:
+void count_perimeter() {perimeter = side1+side2+side3;}
+void count_square() {//square = 0.5*side1*side2*sin(angle1*M_PI/180);
 
-    LibraryTriangle (double t_side1, double t_side2, double t_side3): side1(t_side1), side2(t_side2), side3(t_side3)
-    {
-        perimeter=count_perimeter();
-        square=count_square();
-    }
+double hp = ((side1+side2+side3)/2);
+square = sqrt(abs(hp*(hp-side1)*(hp-side2)*(hp-side3)));}
+void count_side1_sidesangle();
+void count_angles();
 
-    LibraryTriangle (double t_side1, double t_side2, double t_side3, double t_angle1, double t_angle2, double t_angle3): side1(t_side1), side2(t_side2), side3(t_side3), angle1(t_angle1), angle2(t_angle2), angle3(t_angle3)
-    {
-        perimeter=count_perimeter();
-        square=count_square();
-    }
+void count_on_2angle_1side();
+void count_on_1angle_2side();
+int count_3angle_on_3side();
+Triangle();
+Triangle (double t_side1, double t_side2, double t_side3);
+Triangle(double t_size1, double t_size2, int t_angle1);
+void set_side1(double a);
+void set_side2(double a);
+void set_side3(double a);
+void set_angle1(double b);
+void set_angle2(double b);
+void set_angle3(double b);
 
-    void set_side1(double a);
-    void set_side2(double a);
-    void set_side3(double a);
-    void set_angle1(double b);
-    void set_angle2(double b);
-    void set_angle3(double b);
+double get_side1(){return side1;}
+double get_side2(){return side2;}
+double get_side3(){return side3;}
+int get_angle1(){return angle1;}
+int get_angle2(){return angle2;}
+int get_angle3(){return angle3;}
 
-    double get_side1(){return side1;}
-    double get_side2(){return side2;}
-    double get_side3(){return side3;}
-    double get_angle1(){return angle1;}
-    double get_angle2(){return angle2;}
-    double get_angle3(){return angle3;}
-
-
-    double count_perimeter();
-    double count_square();
-    double count_side1_sidesangle();
-    double count_third_angle();
-    void count_angles_cos();
-    void count_on_2angle_1side();
-    double count_on_1angle_2side();
-    double count_3angle_on_3side();
-
-
-    double  get_perimeter(){return perimeter;}
-    double  get_square(){return square;}
-    void show_figure ();
-
-    QString get_name() {return name;}
-    ~LibraryTriangle();
+double get_perimeter(){ return perimeter; }
+double get_square(){ return square; }
+QString get_name() { return name; }
+~Triangle() {}
 };
 #endif // LIBRARYTRIANGLE_H
